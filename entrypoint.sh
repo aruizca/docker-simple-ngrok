c5a6a47d2d5fd6020609298ad85a8a4ae5502e7c
Angel Ruiz <angel@comalatech.com>
2017-06-09 12:29:06 +0200

    Fixed extra quote

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 echo "ngrok v2 running with the following settings:"
 echo "subdomain: ${NGROK_SUBDOMAIN}"
 echo "foward host: ${NGROK_FORWARD_HOST}"
-echo "port: ${NGROK_PORT}""
+echo "port: ${NGROK_PORT}"
 
 ./ngrok http -subdomain=${NGROK_SUBDOMAIN} ${NGROK_FORWARD_HOST}:${NGROK_PORT}
